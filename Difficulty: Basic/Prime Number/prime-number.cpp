607dbe69ee40f39c24f719760ca2eba80e37ef84
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-06-27 09:29:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.cpp
+++ Difficulty: Basic/Prime Number/prime-number.cpp
@@ -1,40 +1,16 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     bool isPrime(int n) {
         // code here
-        if(n<=1) return false;
-        if(n<=2) return true;
-        int count =0;
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0) count++;
+        int count = 0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                count++;
+                if(n/i != i) count++;
+            }
+            if(count>2) break;
         }
-        if(count==0) return true;
+        if(count==2) return true;
         else return false;
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        Solution ob;
-        cout << (ob.isPrime(N) ? "true" : "false") << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
