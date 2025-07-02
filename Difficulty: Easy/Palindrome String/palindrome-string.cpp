328ff42a258cc2bfc4bc698cb5e3e43a4ba5b6a9
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-07-02 05:48:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Function to check if a string is a palindrome.
+    bool isPalindrome(string& s) {
+        // code here
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
