022180bd31fb560413bc0db7c745c3e83dcfcd5d
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-08-19 09:48:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
   public:
     void insertionSort(vector<int>& arr) {
@@ -13,6 +14,25 @@ class Solution {
             }
             
         }
+       
+    }
+};
+*/
+class Solution {
+  public:
+    void recInsertionSort(vector<int>&arr ,int i,int n){
+        if(i==n) return;
+        int j = i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+        recInsertionSort(arr,i+1,n);
+    }
+    void insertionSort(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        recInsertionSort(arr, 0 ,n);
        
     }
 };
