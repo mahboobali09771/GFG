b550ba35be9c919185792b33ccb890b05406c03f
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-08-21 07:03:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low<high){
+        int pIdx = partition(arr, low ,high);
+        quickSort(arr,low,pIdx-1);
+        quickSort(arr,pIdx+1,high);
+        }
+    }
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        
+        // code here
+         int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i<j){
+            while(arr[i] <= pivot && i<= high-1)  i++;
+            while(arr[j] > pivot && j>=low+1) j--;
+            if(i<j) swap(arr[i],arr[j]);
+        }
+        swap(arr[low],arr[j]);
+        return j;
+    }
+};
